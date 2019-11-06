83b2f0fce6a2bca70400091b75850ec4b2266035
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-11-06 20:49:13 +0000

    Creat Home component main

---
+++ src/Home.js
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+import "./home.css";
+export class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tickets: [],
+      thursdayOffer: "",
+      ticketType: "standard",
+      ticketPrice: 0,
+      total: 0,
+      selectedReal3D: false,
+      selectedIMAX: false
+    };
+  }
+
+  componentDidMount = () => {
+    this.calculateTotal();
+  };
+
+  calculateTotal = () => {
+    const ticketTYpe = this.state.ticketType;
+    const selectedIMAX = this.state.selectedIMAX;
+    const selectedReal3D = this.state.selectedReal3D;
+
+    let price = 0;
+    if (ticketTYpe === "standard") {
+      price = 7.9;
+    } else if (ticketTYpe === "concession") {
+      price = 5.4;
+    }
+
+    this.setState({
+      ticketPrice: price
+    });
+    let total = 0;
+    if (selectedIMAX && selectedReal3D) {
+      total += price + 1.5 + 0.9;
+    } else if (selectedReal3D) {
+      total += price + 0.9;
+    } else if (selectedIMAX) {
+      total += price + 1.5;
+    }
+    this.setState({
+      total: total.toFixed(2)
+    });
+  };
+
+  handleChange = e => {
+    this.setState(
+      {
+        ticketType: e.target.value
+      },
+      this.calculateTotal
+    );
+  };
+
+  handleInputChangeRel = event => {
+    this.setState(prev => {
+      return {
+        selectedReal3D: !prev.selectedReal3D
+      };
+    }, this.calculateTotal);
+  };
+  handleInputChangeForImax = event => {
+    this.setState(prev => {
+      return {
+        selectedIMAX: !prev.selectedIMAX
+      };
+    }, this.calculateTotal);
+  };
+  render() {
+    return (
+      <form className="form">
+        <label>Pick your ticket:</label>
+
+        <select value={this.state.ticketType} onChange={this.handleChange}>
+          <option value="standard">Standard</option>
+          <option value="concession">Concession</option>
+        </select>
+        <h2> The Ticket price is : £ {this.state.ticketPrice}</h2>
+
+        <label>Real3D: </label>
+        <input
+          name="Real3D"
+          type="checkbox"
+          checked={this.state.selectedReal3D}
+          onChange={this.handleInputChangeRel}
+        />
+
+        <br />
+        <label>IMAX:</label>
+        <input
+          name="IMAX"
+          type="checkbox"
+          checked={this.state.selectedIMAX}
+          onChange={this.handleInputChangeForImax}
+        />
+
+        <h2>The total is : £ {this.state.total}</h2>
+        <button>Add Tickets</button>
+        <h1>{this.state.thursdayOffer}</h1>
+      </form>
+    );
+  }
+}
+
+export default Home;
---
+++ src/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./navbar.css";
+
+export default function NavBar() {
+  return (
+    <div>
+      <header className="App-header">
+        <h1 className="App-title">Cinema Ticket System</h1>
+      </header>
+    </div>
+  );
+}
