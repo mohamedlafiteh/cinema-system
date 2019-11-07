c792a28cd5c996421085414b158324a4e9c01866
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-11-07 20:53:04 +0000

    Add some note to the about page

--- src/About.js
+++ src/About.js
@@ -14,9 +14,10 @@ export default function About() {
         the total. <br /> <pre /> 2- I am working on the Add ticket and the
         Thursday offer and the ticket totaling which will finish it by tomorrow
         <br />
-        and will push the changes to the gihub I am also going to add some unit
-        testing for the code, and some css. <br />I will add router and refactor
-        the code into different components
+        as I didn't have enough time today and will push the changes to the
+        gihub I am also going to add some unit testing <br />
+        for the code, and some css. <br />I will add router and refactor the
+        code into different components
       </p>
       <h3 className="text">Mohamed Lafiteh</h3>
     </div>
